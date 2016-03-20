08bf532554095dbd27b13c0638ba7dae63988aa4
pranav91690 <pranav91690@gmail.com>
2016-03-19 22:45:17 -0400

    Probably good .gitignore

---
+++ StockUp/rideDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  rideDetailViewController.swift
+//  StockUp
+//
+//  Created by Pranav Achanta on 3/18/16.
+//  Copyright © 2016 Franky Liang. All rights reserved.
+//
+
+import UIKit
+
+class rideDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onCancel(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
